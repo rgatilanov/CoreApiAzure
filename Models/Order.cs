e5f9f765d0df35fccadda64228f231dd497c7bf2
Gerardo <Villalobos>
2020-02-08 09:27:11 -0600

    Model Orderl

---
+++ Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApiAzure.Models
+{
+    public class Order
+    {
+        public long OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public long EmployeeID { get; set; }
+        public Double Freight { get; set; }
+        public string ShipCity { get; set; }
+        public bool Verified { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string ShipName { get; set; }
+        public string ShipCountry { get; set; }
+        public DateTime ShippedDate { get; set; }
+        public string ShipAddress { get; set; }
+    }
+}
